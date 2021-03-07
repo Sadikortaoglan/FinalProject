dd32b26995e47f5b61133b850421a5a03a70c058
Onelife <ortcellad@gmail.com>
2021-03-07 22:07:52 +0300

    Eğitimde yapılanlar

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameIncalid = "Ürün ismi gecersiz";
+        internal static string MaintenanceTime = "Sistem Bakımda";
+        internal static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride 10 fazla ürün olamaz ";
+        public static string ProductNameAlreadyExists = "Aynı isimde ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiiniz Yok";
+        internal static string UserRegistered;
+        internal static User UserNotFound;
+        internal static User PasswordError;
+        internal static string SuccessfulLogin;
+        internal static string AccessTokenCreated;
+
+        public static string UserAlreadyExists { get; internal set; }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,31 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                   return result.ToList();
+            }
+        }
+     
+    }
+}
